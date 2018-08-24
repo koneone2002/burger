7f4043d00c3d875dff35e0b62bc26735aec514e1
Katie <katie@imagings-MacBook-Pro.local>
2018-08-23 22:58:08 -0700

    lots of new stuff

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+// Serve static content for the app from the "public" directory
+app.use(express.static("public"));
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
